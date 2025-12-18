a4774af1f8229b661c30102a53a7ffb53f7f3aff
adindaagustinmargarets <widy45rr@gmail.com>
2025-12-19 05:45:05 +0700

    temp

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    public function index()
+    {
+        $user_count = User::count();
+        $user = User::all();
+        return response()->json([
+            'message' => 'This is the user index route.',
+            'total_users' => $user_count,
+            'data' => $user
+        ]);
+    }
+}
